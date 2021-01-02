2068af9f2d6f3072b57aa486d0e9eea88ab960e8
philogynic <bimogame161@gmail.com>
2021-01-02 18:13:01 +0700

    fixing some statusResponse undefined error

--- functions/snapNotification.js
+++ functions/snapNotification.js
@@ -8,13 +8,14 @@ exports.handler = async (event, context, callback) => {
         clientKey: 'SB-Mid-client-OFR_K5ME8oifxlc6'
     });
     
-    apiClient.transaction.notification(event)
+    statusResponse = await apiClient.transaction.notification(event)
         .then((statusResponse) => {
             let orderId = statusResponse.order_id;
             let transactionStatus = statusResponse.transaction_status;
             let fraudStatus = statusResponse.fraud_status;
 
             console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
+            return statusResponse
         })
 
     return {
