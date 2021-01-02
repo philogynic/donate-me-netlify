896ca26ad938131d38957fa6e199cc44e3338e0d
philogynic <bimogame161@gmail.com>
2021-01-02 17:56:27 +0700

    try receive notification by logging

---
+++ functions/snapNotification.js
@@ -0,0 +1,24 @@
+const midtransClient = require('midtrans-client');
+
+exports.handler = async (event, context, callback) => {
+    let apiClient = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction : false,
+        serverKey : 'SB-Mid-server-RPIkHxFpG3BBH1zazgRyYwEd',
+        clientKey: 'SB-Mid-client-OFR_K5ME8oifxlc6'
+    });
+    
+    apiClient.transaction.notification(event)
+        .then((statusResponse) => {
+            let orderId = statusResponse.order_id;
+            let transactionStatus = statusResponse.transaction_status;
+            let fraudStatus = statusResponse.fraud_status;
+
+            console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
+        })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({'transactionToken': statusResponse})
+    }
+}
